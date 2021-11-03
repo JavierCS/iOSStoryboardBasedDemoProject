673d26034dc810a10a7b37f4dd59e658ed4f482b
Javier Cruz Santiago <javiercruzsantiago@gmail.com>
2021-11-03 17:20:53 -0600

    [FIX] - Fixed Pod files

--- Pods/Target Support Files/Pods-iOSStoryboardBasedDemoProject-iOSStoryboardBasedDemoProjectUITests/Pods-iOSStoryboardBasedDemoProject-iOSStoryboardBasedDemoProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSStoryboardBasedDemoProject_iOSStoryboardBasedDemoProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSStoryboardBasedDemoProject_iOSStoryboardBasedDemoProjectUITests
-@end
