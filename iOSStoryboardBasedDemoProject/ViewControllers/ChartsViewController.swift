49b713d0a9dce2acdfb2e9c014f0c912e7a12c5c
Javier Cruz Santiago <javiercruzsantiago@gmail.com>
2021-11-03 18:28:00 -0600

    [UPDATE] - Modified Chart View in order to show only one pie chart as Banco Azteca Needed

--- iOSStoryboardBasedDemoProject/ViewControllers/ChartsViewController.swift
+++ iOSStoryboardBasedDemoProject/ViewControllers/ChartsViewController.swift
@@ -29,7 +29,7 @@ class ChartsViewController: UIViewController {
 //MARK: - UITableViewDataSource Management
 extension ChartsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- iOSStoryboardBasedDemoProject/XIBViews/PieChartTableViewCell/PieChartTableViewCell.swift
+++ iOSStoryboardBasedDemoProject/XIBViews/PieChartTableViewCell/PieChartTableViewCell.swift
@@ -15,15 +15,23 @@ class PieChartTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let carEntry = PieChartDataEntry(value: 1200, label: "Auto", icon: NSUIImage(named: "car")?.withTintColor(.white))
+        let cinemaEntry = PieChartDataEntry(value: 750, label: "Entretenimiento", icon: NSUIImage(named: "cinema")?.withTintColor(.white))
+        let homeEntry = PieChartDataEntry(value: 2500, label: "Hogar", icon: NSUIImage(named: "home")?.withTintColor(.white))
+        
         let entries = [
-            PieChartDataEntry(value: 40, label: "TDC"),
-            PieChartDataEntry(value: 50, label: "TDD")
+            carEntry,
+            cinemaEntry,
+            homeEntry
         ]
         
         let dataSet = PieChartDataSet(entries: entries, label: "Data Set Label")
-        dataSet.colors = [UIColor.blue, UIColor.green]
+        dataSet.drawValuesEnabled = false
+        dataSet.colors = [UIColor.blue, UIColor.red, UIColor.orange]
         let data = PieChartData(dataSet: dataSet)
         
+        self.pieChartView.centerText = "Has gastado \n$4000.00"
+        self.pieChartView.drawEntryLabelsEnabled = false
         self.pieChartView.data = data
     }
 
